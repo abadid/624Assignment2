1c8289034135cb910b4877fe79f1f6e630a0bf36
konstantinosx <kostasx@cs.umd.edu>
2019-09-12 18:31:50 -0400

    small typos and change readme to point to heave2 instead of junkfood

--- txn/txn_processor.h
+++ txn/txn_processor.h
@@ -24,122 +24,121 @@ using std::string;
 // The TxnProcessor supports five different execution modes, corresponding to
 // the four parts of assignment 2, plus a simple serial (non-concurrent) mode.
 enum CCMode {
-  SERIAL = 0,                  // Serial transaction execution (no concurrency)
-  LOCKING_EXCLUSIVE_ONLY = 1,  // Part 1A
-  LOCKING = 2,                 // Part 1B
-  OCC = 3,                     // Part 2
-  P_OCC = 4,                   // Part 3
-  MVCC = 5,
+        SERIAL = 0,                 // Serial transaction execution (no concurrency)
+        LOCKING_EXCLUSIVE_ONLY = 1, // Part 1A
+        LOCKING = 2,                // Part 1B
+        OCC = 3,                    // Part 2
+        P_OCC = 4,                  // Part 3
+        MVCC = 5,                   // Part 4
 };
 
 // Returns a human-readable string naming of the providing mode.
 string ModeToString(CCMode mode);
 
 class TxnProcessor {
- public:
-  // The TxnProcessor's constructor starts the TxnProcessor running in the
-  // background.
-  explicit TxnProcessor(CCMode mode);
+public:
+// The TxnProcessor's constructor starts the TxnProcessor running in the
+// background.
+explicit TxnProcessor(CCMode mode);
 
-  // The TxnProcessor's destructor stops all background threads and deallocates
-  // all objects currently owned by the TxnProcessor, except for Txn objects.
-  ~TxnProcessor();
+// The TxnProcessor's destructor stops all background threads and deallocates
+// all objects currently owned by the TxnProcessor, except for Txn objects.
+~TxnProcessor();
 
-  // Registers a new txn request to be executed by the TxnProcessor.
-  // Ownership of '*txn' is transfered to the TxnProcessor.
-  void NewTxnRequest(Txn* txn);
+// Registers a new txn request to be executed by the TxnProcessor.
+// Ownership of '*txn' is transfered to the TxnProcessor.
+void NewTxnRequest(Txn* txn);
 
-  // Returns a pointer to the next COMMITTED or ABORTED Txn. The caller takes
-  // ownership of the returned Txn.
-  Txn* GetTxnResult();
+// Returns a pointer to the next COMMITTED or ABORTED Txn. The caller takes
+// ownership of the returned Txn.
+Txn* GetTxnResult();
 
-  // Main loop implementing all concurrency control/thread scheduling.
-  void RunScheduler();
-  
-  static void* StartScheduler(void * arg);
-  
- private:
+// Main loop implementing all concurrency control/thread scheduling.
+void RunScheduler();
 
-  // Serial validation
-  bool SerialValidate(Txn *txn);
+static void* StartScheduler(void * arg);
 
-  // Parallel executtion/validation for OCC
-  void ExecuteTxnParallel(Txn *txn);
+private:
 
-  // Serial version of scheduler.
-  void RunSerialScheduler();
+// Serial validation
+bool SerialValidate(Txn *txn);
 
-  // Locking version of scheduler.
-  void RunLockingScheduler();
+// Parallel executtion/validation for OCC
+void ExecuteTxnParallel(Txn *txn);
 
-  // OCC version of scheduler.
-  void RunOCCScheduler();
+// Serial version of scheduler.
+void RunSerialScheduler();
 
-  // OCC version of scheduler with parallel validation.
-  void RunOCCParallelScheduler();
-  
-  // MVCC version of scheduler.
-  void RunMVCCScheduler();
+// Locking version of scheduler.
+void RunLockingScheduler();
 
-  // Performs all reads required to execute the transaction, then executes the
-  // transaction logic.
-  void ExecuteTxn(Txn* txn);
+// OCC version of scheduler.
+void RunOCCScheduler();
 
-  // Applies all writes performed by '*txn' to 'storage_'.
-  //
-  // Requires: txn->Status() is COMPLETED_C.
-  void ApplyWrites(Txn* txn);
-  
-  // The following functions are for MVCC
-  void MVCCExecuteTxn(Txn* txn);
-    
-  bool MVCCCheckWrites(Txn* txn);
+// OCC version of scheduler with parallel validation.
+void RunOCCParallelScheduler();
 
-  void MVCCLockWriteKeys(Txn* txn);
+// MVCC version of scheduler.
+void RunMVCCScheduler();
 
-  void MVCCUnlockWriteKeys(Txn* txn);
-  
-  void GarbageCollection();
-  
-  // Concurrency control mechanism the TxnProcessor is currently using.
-  CCMode mode_;
+// Performs all reads required to execute the transaction, then executes the
+// transaction logic.
+void ExecuteTxn(Txn* txn);
 
-  // Thread pool managing all threads used by TxnProcessor.
-  StaticThreadPool tp_;
+// Applies all writes performed by '*txn' to 'storage_'.
+//
+// Requires: txn->Status() is COMPLETED_C.
+void ApplyWrites(Txn* txn);
 
-  // Data storage used for all modes.
-  Storage* storage_;
+// The following functions are for MVCC
+void MVCCExecuteTxn(Txn* txn);
 
-  // Next valid unique_id, and a mutex to guard incoming txn requests.
-  int next_unique_id_;
-  Mutex mutex_;
+bool MVCCCheckWrites(Txn* txn);
 
-  // Queue of incoming transaction requests.
-  AtomicQueue<Txn*> txn_requests_;
+void MVCCLockWriteKeys(Txn* txn);
 
-  // Queue of txns that have acquired all locks and are ready to be executed.
-  //
-  // Does not need to be atomic because RunScheduler is the only thread that
-  // will ever access this queue.
-  deque<Txn*> ready_txns_;
+void MVCCUnlockWriteKeys(Txn* txn);
 
-  // Queue of completed (but not yet committed/aborted) transactions.
-  AtomicQueue<Txn*> completed_txns_;
+void GarbageCollection();
 
-  // Queue of transaction results (already committed or aborted) to be returned
-  // to client.
-  AtomicQueue<Txn*> txn_results_;
-  
-  // Set of transactions that are currently in the process of parallel
-  // validation.
-  AtomicSet<Txn*> active_set_;
+// Concurrency control mechanism the TxnProcessor is currently using.
+CCMode mode_;
 
-  // Used it for critical section in parallel occ.
-  Mutex active_set_mutex_;
+// Thread pool managing all threads used by TxnProcessor.
+StaticThreadPool tp_;
 
-  // Lock Manager used for LOCKING concurrency implementations.
-  LockManager* lm_;
+// Data storage used for all modes.
+Storage* storage_;
+
+// Next valid unique_id, and a mutex to guard incoming txn requests.
+int next_unique_id_;
+Mutex mutex_;
+
+// Queue of incoming transaction requests.
+AtomicQueue<Txn*> txn_requests_;
+
+// Queue of txns that have acquired all locks and are ready to be executed.
+//
+// Does not need to be atomic because RunScheduler is the only thread that
+// will ever access this queue.
+deque<Txn*> ready_txns_;
+
+// Queue of completed (but not yet committed/aborted) transactions.
+AtomicQueue<Txn*> completed_txns_;
+
+// Queue of transaction results (already committed or aborted) to be returned
+// to client.
+AtomicQueue<Txn*> txn_results_;
+
+// Set of transactions that are currently in the process of parallel
+// validation.
+AtomicSet<Txn*> active_set_;
+
+// Used it for critical section in parallel occ.
+Mutex active_set_mutex_;
+
+// Lock Manager used for LOCKING concurrency implementations.
+LockManager* lm_;
 };
 
 #endif  // _TXN_PROCESSOR_H_
-
